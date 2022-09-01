5e5bf26443b17176c58a93197d487acd7ab9069e
QGPike <69122709+QGPike@users.noreply.github.com>
2022-09-01 14:40:43 -0500

    test drive

--- Therapeutic_Gold/settings.lua
+++ Therapeutic_Gold/settings.lua
@@ -18,3 +18,16 @@ mod_settings =
 	}
 }
 
+-- This function is called to display the settings UI for this mod. Your mod's settings wont be visible in the mod settings menu if this function isn't defined correctly.
+function ModSettingsUpdate( init_scope )
+    local old_version = mod_settings_get_version( mod_id )
+    mod_settings_update( mod_id, mod_settings, init_scope )
+end
+
+function ModSettingsGuiCount()
+    return mod_settings_gui_count( mod_id, mod_settings )
+end
+
+function ModSettingsGui( gui, in_main_menu )
+    mod_settings_gui( mod_id, mod_settings, gui, in_main_menu )
+end

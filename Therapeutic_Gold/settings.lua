8df376080a105ca3e2873c698d6bd9bdded68c5a
QGPike <69122709+QGPike@users.noreply.github.com>
2022-09-01 14:22:05 -0500

    modified:   Therapeutic_Gold/settings.lua

--- Therapeutic_Gold/settings.lua
+++ Therapeutic_Gold/settings.lua
@@ -3,4 +3,18 @@ dofile_once("data/scripts/lib/mod_settings.lua")
 
 local mod_id = "Therapeutic_Gold"
 mod_settings_version = 1
+mod_settings =
+{
+	{
+		id = "pos_x",
+		ui_name = "Horizontal position",
+		ui_description = "",
+		value_default = 2,
+		value_min = 0,
+		value_max = 1000,
+		value_display_multiplier = 1,
+		value_display_formatting = " x = $0",
+		scope = MOD_SETTING_SCOPE_RUNTIME,
+	}
+}
 

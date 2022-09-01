9619306a11582e54e2099cf12a168d01384c963a
QGPike <69122709+QGPike@users.noreply.github.com>
2022-09-01 14:36:55 -0500

    setting

--- Therapeutic_Gold/settings.lua
+++ Therapeutic_Gold/settings.lua
@@ -9,11 +9,11 @@ mod_settings =
 		id = "regen_scalar",
 		ui_name = "Regen Multiplier",
 		ui_description = "The amount of health gained per 1 gold earned",
-		value_default = 0.016,
+		value_default = 0.25,
 		value_min = 0,
-		value_max = 1,
+		value_max = 5,
 		value_display_multiplier = 1,
-		value_display_formatting = " x = $0",
+		value_display_formatting = "",
 		scope = MOD_SETTING_SCOPE_RUNTIME,
 	}
 }

d35357cd5609bffb08d067e424d66e1a5852ed85
QGPike <69122709+QGPike@users.noreply.github.com>
2022-09-01 14:16:06 -0500

    check 1

--- Therapeutic_Gold/config.lua
+++ Therapeutic_Gold/config.lua
@@ -1,2 +1,2 @@
-hVal  = 0.016   -- Health that 1 gold gives, 0.04 is 1 pt of health. 
+hVal  = ModSettingGet("")   -- Health that 1 gold gives, 0.04 is 1 pt of health. default: 0.016 
 pGold = 100000000000 -- Should prevent full health load bug, if you have this much gold... Change this I guess

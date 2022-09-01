849997a84b64fd02e8224abe0c878b18ab70b5c2
QGPike <69122709+QGPike@users.noreply.github.com>
2022-09-01 15:39:28 -0500

    test run

--- Therapeutic_Gold/config.lua
+++ Therapeutic_Gold/config.lua
@@ -1,2 +1,2 @@
-hVal  = tonumber(ModSettingGet("Therapeutic_Gold.regen_scalar"))--/25   -- Health that 1 gold gives, 0.04 is 1 pt of health. 1/25 = 0.04, this converts the value properly
+hVal  = tonumber(ModSettingGet("Therapeutic_Gold.regen_scalar"))/25   -- Health that 1 gold gives, 0.04 is 1 pt of health. 1/25 = 0.04, this converts the value properly
 pGold = 100000000000 -- Should prevent full health load bug, if you have this much gold... Change this I guess
